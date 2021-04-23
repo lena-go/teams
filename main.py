15e0044834ca1b7f0454e33909813d1778853f85
Elena Gordeeva <gordeewa29@gmail.com>
2021-04-23 09:06:52 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import csv
+import datetime
+
+
+class Meeting:
+    def __init__(self):
+        self.timeline = [0 for i in range(minutes_of_meeting)]
+
+
+minutes_of_meeting = 90  # function
+timeline = [0 for i in range(minutes_of_meeting)]
+
+def mark_timeline(meeting_time, member, timeline):
+    pass
+
+def compute_meeting_period(meeting_time: 'datetime', timeline: [int], members_time: ['datetime']):
+    for member in members_time:
+        mark_timeline(meeting_time, member, timeline)
+
+
+if __name__ == '__main__':
+    file_name = 'meetingAttendanceReport(General) (3).csv'
+    with open(file_name, newline='', encoding='utf-16') as csvfile:
+        teamsreader = csv.reader(csvfile, delimiter='\t')
+        for row in teamsreader:
+            print(', '.join(row))
